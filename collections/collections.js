a984ab0db961d06105b2379fe233ffab7b8af93a
Cesar <cesar6134@gmail.com>
2014-08-07 07:10:43 +0800

    Initialize collections

---
+++ collections/collections.js
@@ -0,0 +1,6 @@
+Categories = new Meteor.Collection('categories');
+SubCategories = new Meteor.Collection('subCategories');
+Products = new Meteor.Collection('products');
+Customers = new Meteor.Collection('customers');
+Cart = new Meteor.Collection('cart');
+CartItems = new Meteor.Collection('cartItems');
